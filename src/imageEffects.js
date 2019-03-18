db661f482aef824998ef8f1c82ace5d7e060f2f6
cdrani <cdrani@users.noreply.github.com>
2019-03-18 03:36:31 -0600

    add flannel, night-vision, pencil, and emboss effects

--- src/imageEffects.js
+++ src/imageEffects.js
@@ -1,12 +1,31 @@
 const imageEffects = src => ({
-  pencil: {
+  "night-vision": {
+    backgroundImage: `url(${src}), radial-gradient(#0F0, #000), repeating-linear-gradient(transparent 0, rgba(0, 0, 0, 0.1) 2.5px, transparent 5px)`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundBlendMode: "overlay"
+  },
+  "pencil": {
     backgroundImage: `url(${src}), url(${src})`,
     backgroundBlendMode: "difference",
     backgroundPosition:
       "calc(50% - 1px) calc(50% - 1px), calc(50% + 1px) calc(50% + 1px)",
     filter: "brightness(2) invert(1) grayscale(1)",
     boxShadow: "inset 0 0 0 1px black"
+  },
+  "emboss": {
+    backgroundImage: `url(${src}), url(${src}), url(${src})`,
+    backgroundBlendMode: "difference, screen",
+    backgroundPosition:
+      "calc(50% - 1px) calc(50% - 1px), calc(50% + 1px) calc(50% + 1px), center",
+    filter: "brightness(2) invert(1) grayscale(1)"
+  },
+  "flannel": {
+    backgroundImage: `url(${src}), url(${src}), url(${src})`,
+    backgroundPosition: "center",
+    backgroundSize: "100%, 100000% 100%, 100% 100000%",
+    backgroundBlendMode: "overlay"
   }
-})
+});
 
-export default imageEffects
+export default imageEffects;
--- src/index.js
+++ src/index.js
@@ -1,50 +1,50 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import styled from 'styled-components'
-import imageEffects from './imageEffects'
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import imageEffects from "./imageEffects";
 
-
-const ImageEffect = ({ src, effect }) => {
-  const Image = styled.img`
-    background-image: ${props => `url(${props.src})`};
+const ImageEffect = ({ url, effect }) => {
+  const Image = styled.div`
+    width: 600px;
+    height: 300px;
+    background-image: ${props => `url(${props.url})`};
     background-size: cover;
     background-position: center;
-  `
+  `;
 
-  const Effect = styled(Image)(props => imageEffects(src)[effect])
-  return <Effect src={src} />
-}
-
+  const Effect = styled(Image)(props => imageEffects(props.url)[effect]);
+  return <Effect url={url} />;
+};
 
 ImageEffect.propTypes = {
-  src: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
   effect: PropTypes.oneOf([
-    'none',
-    'pencil',
-    'watercolor',
-    'emboss',
-    'colored-pencil',
-    'chalkboard',
-    'colored-chalkboard',
-    'airbrush',
-    'hallucination',
-    'flannel',
-    'low-ink-v',
-    'low-ink-h',
-    'collage',
-    'mosaic',
-    'photo-border',
-    'infrared',
-    'night-vision',
-    'warhol',
-    'selective-color',
-    'mirror-h',
-    'mirror-v'
+    "none",
+    "pencil",
+    "watercolor",
+    "emboss",
+    "colored-pencil",
+    "chalkboard",
+    "colored-chalkboard",
+    "airbrush",
+    "hallucination",
+    "flannel",
+    "low-ink-v",
+    "low-ink-h",
+    "collage",
+    "mosaic",
+    "photo-border",
+    "infrared",
+    "night-vision",
+    "warhol",
+    "selective-color",
+    "mirror-h",
+    "mirror-v"
   ])
-}
+};
 
 ImageEffect.defaultProps = {
-  effect: 'none'
-}
+  effect: "none"
+};
 
-export default ImageEffect
+export default ImageEffect;
