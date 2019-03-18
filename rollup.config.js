3e1f9e9a8658fcdcabcc3411971e7815f67af3c3
cdrani <cdrani@users.noreply.github.com>
2019-03-17 22:11:28 -0600

    setup rollup config

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+
+import pkg from './package.json'
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'es'
+    }
+  ],
+  plugins: [
+    babel({
+      runtimeHelpers: true,
+      exclude: 'node_modules/**'
+    }),
+    resolve(),
+    commonjs()
+  ]
+}
