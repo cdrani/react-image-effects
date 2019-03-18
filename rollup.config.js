b6ac4ff8020d0f6020f9989e5252f3462a4766eb
cdrani <cdrani@users.noreply.github.com>
2019-03-17 22:16:11 -0600

    setup prod and dev environments

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,10 @@
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
+import replace from 'rollup-plugin-replace'
 import resolve from 'rollup-plugin-node-resolve'
+
+const NODE_ENV = process.env.NODE_ENV || 'development'
+const outputFile = NODE_ENV === 'production' ? './lib/prod.js' : './lib/dev.js'
 
 import pkg from './package.json'
 export default {
@@ -16,6 +20,9 @@ export default {
     }
   ],
   plugins: [
+    replace({
+      "process.env.NODE_ENV": JSON.stringify(NODE_ENV)
+    }),
     babel({
       runtimeHelpers: true,
       exclude: 'node_modules/**'
