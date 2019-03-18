3ed7cb327ec5393813fed38538750de30d40cfe2
cdrani <cdrani@users.noreply.github.com>
2019-03-18 17:19:11 -0600

    add width and height props

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import imageEffects from "./imageEffects";
 
-const ImageEffect = ({ url, effect }) => {
+const ImageEffect = ({ url, effect, width, height }) => {
   const Image = styled.div`
-    width: 600px;
-    height: 300px;
+    width: ${width};
+    height: ${height};
     background-image: ${props => `url(${props.url})`};
     background-size: cover;
     background-position: center;
   `;
 
-  const effectObject = imageEffects(url)[effect]
+  const effectObject = imageEffects(url)[effect];
 
   const Effect = styled(Image)`
-  ${effectObject}
-  `
+    ${effectObject}
+  `;
   return <Effect url={url} />;
 };
 
@@ -44,11 +44,15 @@ ImageEffect.propTypes = {
     "selective-color",
     "mirror-h",
     "mirror-v"
-  ])
+  ]),
+  width: PropTypes.string,
+  height: PropTypes.string
 };
 
 ImageEffect.defaultProps = {
-  effect: "none"
+  effect: "none",
+  width: "600px",
+  height: "300px"
 };
 
 export default ImageEffect;
