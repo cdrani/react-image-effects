d65b8790e465f6c483b7a8c4c0a3457c91dccac5
cdrani <cdrani@users.noreply.github.com>
2019-03-19 09:23:15 -0600

    prettify code

--- src/imageEffects.js
+++ src/imageEffects.js
@@ -1,234 +1,234 @@
 const imageEffects = src => ({
   airbrush: {
-    position: "relative",
-    overflow: "hidden",
-    "::after": {
-      display: "block",
+    position: 'relative',
+    overflow: 'hidden',
+    '::after': {
+      display: 'block',
       content: "''",
-      position: "absolute",
+      position: 'absolute',
       top: 0,
       left: 0,
       right: 0,
       bottom: 0,
-      background: "inherit",
-      filter: "brightness(1.5) saturate(100) blur(5px) contrast(5)",
-      mixBlendMode: "multiply"
+      background: 'inherit',
+      filter: 'brightness(1.5) saturate(100) blur(5px) contrast(5)',
+      mixBlendMode: 'multiply'
     }
   },
   chalkboard: {
     backgroundImage: `url(${src}), url(${src})`,
-    backgroundBlendMode: "difference",
+    backgroundBlendMode: 'difference',
     backgroundPosition:
-      "calc(50% - 1px) calc(50% - 1px), calc(50% + 1px) calc(50% + 1px)",
-    filter: "brightness(1.5) grayscale(1)"
+      'calc(50% - 1px) calc(50% - 1px), calc(50% + 1px) calc(50% + 1px)',
+    filter: 'brightness(1.5) grayscale(1)'
   },
-  "colored-chalkboard": {
+  'colored-chalkboard': {
     backgroundImage: `url(${src}), url(${src}), url(${src})`,
-    backgroundSize: "cover",
+    backgroundSize: 'cover',
     backgroundPosition:
-      "calc(50% - 1px) calc(50% - 1px), calc(50% + 1px) calc(50% + 1px), center",
-    backgroundBlendMode: "color, difference",
-    filter: "brightness(2)"
+      'calc(50% - 1px) calc(50% - 1px), calc(50% + 1px) calc(50% + 1px), center',
+    backgroundBlendMode: 'color, difference',
+    filter: 'brightness(2)'
   },
   collage: {
     backgroundImage: `url(${src}), url(${src}), url(${src}), url(${src}), url(${src}), url(${src})`,
-    backgroundSize: "200%, 80%, 60%, 50%, 40%, 100%",
-    backgroundPosition: "50%, 80%, 30%, 0",
-    backgroundBlendMode: "overlay",
-    backgroundRepeat: "no-repeat"
+    backgroundSize: '200%, 80%, 60%, 50%, 40%, 100%',
+    backgroundPosition: '50%, 80%, 30%, 0',
+    backgroundBlendMode: 'overlay',
+    backgroundRepeat: 'no-repeat'
   },
-  "colored-pencil": {
-    position: "relative",
-    "::before, ::after": {
-      display: "block",
+  'colored-pencil': {
+    position: 'relative',
+    '::before, ::after': {
+      display: 'block',
       content: "''",
-      position: "absolute",
+      position: 'absolute',
       top: 0,
       left: 0,
       right: 0,
       bottom: 0,
-      backgroundSize: "cover",
-      boxShadow: "inset 0 0 0 1px black"
+      backgroundSize: 'cover',
+      boxShadow: 'inset 0 0 0 1px black'
     },
-    "::before": {
+    '::before': {
       backgroundImage: `url(${src}), url(${src})`,
-      backgroundBlendMode: "difference",
+      backgroundBlendMode: 'difference',
       backgroundPosition:
-        "calc(50% - 1px) calc(50% - 1px), calc(50% + 1px) calc(50% + 1px)",
-      filter: "brightness(2) invert(1) grayscale(1)"
+        'calc(50% - 1px) calc(50% - 1px), calc(50% + 1px) calc(50% + 1px)',
+      filter: 'brightness(2) invert(1) grayscale(1)'
     }
   },
   emboss: {
     backgroundImage: `url(${src}), url(${src}), url(${src})`,
-    backgroundBlendMode: "difference, screen",
+    backgroundBlendMode: 'difference, screen',
     backgroundPosition:
-      "calc(50% - 1px) calc(50% - 1px), calc(50% + 1px) calc(50% + 1px), center",
-    filter: "brightness(2) invert(1) grayscale(1)"
+      'calc(50% - 1px) calc(50% - 1px), calc(50% + 1px) calc(50% + 1px), center',
+    filter: 'brightness(2) invert(1) grayscale(1)'
   },
   flannel: {
     backgroundImage: `url(${src}), url(${src}), url(${src})`,
-    backgroundPosition: "center",
-    backgroundSize: "100%, 100000% 100%, 100% 100000%",
-    backgroundBlendMode: "overlay"
+    backgroundPosition: 'center',
+    backgroundSize: '100%, 100000% 100%, 100% 100000%',
+    backgroundBlendMode: 'overlay'
   },
   hallucination: {
-    position: "relative",
-    overflow: "hidden",
-    backgroundColor: "magenta",
-    backgroundBlendMode: "screen",
-    "::before, ::after": {
-      display: "block",
+    position: 'relative',
+    overflow: 'hidden',
+    backgroundColor: 'magenta',
+    backgroundBlendMode: 'screen',
+    '::before, ::after': {
+      display: 'block',
       content: "''",
-      position: "absolute",
+      position: 'absolute',
       top: 0,
       left: 0,
       right: 0,
       bottom: 0,
-      background: "inherit",
-      mixBlendMode: "multiply",
-      transform: "scale(1.05)"
+      background: 'inherit',
+      mixBlendMode: 'multiply',
+      transform: 'scale(1.05)'
     },
-    "::before": {
-      backgroundColor: "yellow",
-      backgroundBlendMode: "screen",
-      transformOrigin: "top left"
+    '::before': {
+      backgroundColor: 'yellow',
+      backgroundBlendMode: 'screen',
+      transformOrigin: 'top left'
     },
-    "::after": {
-      backgroundColor: "cyan",
-      backgroundBlendMode: "screen",
-      transformOrigin: "bottom right"
+    '::after': {
+      backgroundColor: 'cyan',
+      backgroundBlendMode: 'screen',
+      transformOrigin: 'bottom right'
     }
   },
-  "low-ink-h": {
+  'low-ink-h': {
     backgroundImage: `url(${src}), url(${src}), url(${src})
 `,
-    backgroundSize: "100% 100%, 10000% 100%",
-    backgroundBlendMode: "screen, overlay"
+    backgroundSize: '100% 100%, 10000% 100%',
+    backgroundBlendMode: 'screen, overlay'
   },
-  "low-ink-v": {
+  'low-ink-v': {
     backgroundImage: `url(${src}), url(${src}), url(${src})`,
-    backgroundSize: "100% 100%, 100% 1000%",
-    backgroundBlendMode: "screen, overlay"
+    backgroundSize: '100% 100%, 100% 1000%',
+    backgroundBlendMode: 'screen, overlay'
   },
-  "mirror-h": {
-    position: "relative",
-    "::before, ::after": {
-      display: "block",
+  'mirror-h': {
+    position: 'relative',
+    '::before, ::after': {
+      display: 'block',
       content: "''",
-      position: "absolute",
+      position: 'absolute',
       top: 0,
       bottom: 0,
-      background: "inherit"
+      background: 'inherit'
     },
-    "::before": {
+    '::before': {
       left: 0,
-      right: "50%",
-      transform: "scaleX(-1)"
+      right: '50%',
+      transform: 'scaleX(-1)'
     },
-    "::after": {
-      left: "50%",
+    '::after': {
+      left: '50%',
       right: 0
     }
   },
-  "mirror-v": {
-    position: "relative",
-    "::before, ::after": {
-      display: "block",
+  'mirror-v': {
+    position: 'relative',
+    '::before, ::after': {
+      display: 'block',
       content: "''",
-      position: "absolute",
+      position: 'absolute',
       left: 0,
       right: 0,
-      background: "inherit"
+      background: 'inherit'
     },
-    "::before": {
+    '::before': {
       top: 0,
-      bottom: "50%",
-      transform: "scaleX(-1)"
+      bottom: '50%',
+      transform: 'scaleX(-1)'
     },
-    "::after": {
-      top: "50%",
+    '::after': {
+      top: '50%',
       bottom: 0
     }
   },
   mosaic: {
     backgroundImage: `url(${src}), url(${src})`,
-    backgroundSize: "cover, 5% 5%",
-    backgroundPosition: "center",
-    backgroundBlendMode: "overlay"
+    backgroundSize: 'cover, 5% 5%',
+    backgroundPosition: 'center',
+    backgroundBlendMode: 'overlay'
   },
-  "night-vision": {
+  'night-vision': {
     backgroundImage: `url(${src}), radial-gradient(#0F0, #000), repeating-linear-gradient(transparent 0, rgba(0, 0, 0, 0.1) 2.5px, transparent 5px)`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    backgroundBlendMode: "overlay"
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundBlendMode: 'overlay'
   },
   none: {},
   pencil: {
     backgroundImage: `url(${src}), url(${src})`,
-    backgroundBlendMode: "difference",
+    backgroundBlendMode: 'difference',
     backgroundPosition:
-      "calc(50% - 1px) calc(50% - 1px), calc(50% + 1px) calc(50% + 1px)",
-    filter: "brightness(2) invert(1) grayscale(1)",
-    boxShadow: "inset 0 0 0 1px black"
+      'calc(50% - 1px) calc(50% - 1px), calc(50% + 1px) calc(50% + 1px)',
+    filter: 'brightness(2) invert(1) grayscale(1)',
+    boxShadow: 'inset 0 0 0 1px black'
   },
-  "photo-border": {
+  'photo-border': {
     backgroundImage: `url(${src}), url(${src})`,
-    backgroundPosition: "center",
-    backgroundSize: "60%, 20%",
-    backgroundRepeat: "no-repeat, repeat"
+    backgroundPosition: 'center',
+    backgroundSize: '60%, 20%',
+    backgroundRepeat: 'no-repeat, repeat'
   },
-  "selective-color": {
-    position: "relative",
-    "::before, ::after": {
-      display: "block",
+  'selective-color': {
+    position: 'relative',
+    '::before, ::after': {
+      display: 'block',
       content: "''",
-      position: "absolute",
+      position: 'absolute',
       top: 0,
       left: 0,
       right: 0,
       bottom: 0,
-      background: "inherit",
-      backgroundColor: "red",
-      backgroundBlendMode: "screen",
-      mixBlendMode: "color",
-      filter: "brightness(3)"
+      background: 'inherit',
+      backgroundColor: 'red',
+      backgroundBlendMode: 'screen',
+      mixBlendMode: 'color',
+      filter: 'brightness(3)'
     }
   },
   warhol: {
     backgroundImage: `linear-gradient(#14EBFF 0, #14EBFF 50%, #FFFF70 50%, #FFFF70 100%), linear-gradient(#FF85DA 0, #FF85DA 50%, #AAA 50%, #AAA 100%), url(${src})`,
-    backgroundSize: "50% 100%, 50% 100%, 50% 50%",
-    backgroundPosition: "top left, top right",
-    backgroundRepeat: "no-repeat, no-repeat, repeat",
-    backgroundBlendMode: "color"
+    backgroundSize: '50% 100%, 50% 100%, 50% 50%',
+    backgroundPosition: 'top left, top right',
+    backgroundRepeat: 'no-repeat, no-repeat, repeat',
+    backgroundBlendMode: 'color'
   },
   watercolor: {
-    position: "relative",
-    overflow: "hidden",
-    "::before, ::after": {
-      display: "block",
+    position: 'relative',
+    overflow: 'hidden',
+    '::before, ::after': {
+      display: 'block',
       content: "''",
-      position: "absolute",
+      position: 'absolute',
       top: 0,
       left: 0,
       right: 0,
       bottom: 0,
-      backgroundSize: "cover"
+      backgroundSize: 'cover'
     },
-    "::before": {
+    '::before': {
       backgroundImage: `url(${src}), url(${src})`,
-      backgroundBlendMode: "difference",
+      backgroundBlendMode: 'difference',
       backgroundPosition:
-        "calc(50% - 1px) calc(50% - 1px), calc(50% + 1px) calc(50% + 1px)",
-      filter: "brightness(2) invert(1) grayscale(1)",
-      boxShadow: "inset 0 0 0 1px black"
+        'calc(50% - 1px) calc(50% - 1px), calc(50% + 1px) calc(50% + 1px)',
+      filter: 'brightness(2) invert(1) grayscale(1)',
+      boxShadow: 'inset 0 0 0 1px black'
     },
-    "::after": {
+    '::after': {
       backgroundImage: `url(${src})`,
-      backgroundPosition: "center",
-      mixBlendMode: "multiply",
-      filter: "brightness(1.3) blur(2px) contrast(2)"
+      backgroundPosition: 'center',
+      mixBlendMode: 'multiply',
+      filter: 'brightness(1.3) blur(2px) contrast(2)'
     }
   }
-});
+})
 
-export default imageEffects;
+export default imageEffects
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styled from "styled-components";
-import imageEffects from "./imageEffects";
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import imageEffects from './imageEffects'
 
 const ImageEffect = ({ url, effect, width, height }) => {
   const Image = styled.div`
@@ -10,49 +10,49 @@ const ImageEffect = ({ url, effect, width, height }) => {
     background-image: ${props => `url(${props.url})`};
     background-size: cover;
     background-position: center;
-  `;
+  `
 
-  const effectObject = imageEffects(url)[effect];
+  const effectObject = imageEffects(url)[effect]
 
   const Effect = styled(Image)`
     ${effectObject}
-  `;
-  return <Effect url={url} />;
-};
+  `
+  return <Effect url={url} />
+}
 
 ImageEffect.propTypes = {
   url: PropTypes.string.isRequired,
   effect: PropTypes.oneOf([
-    "none",
-    "pencil",
-    "watercolor",
-    "emboss",
-    "colored-pencil",
-    "chalkboard",
-    "colored-chalkboard",
-    "airbrush",
-    "hallucination",
-    "flannel",
-    "low-ink-v",
-    "low-ink-h",
-    "collage",
-    "mosaic",
-    "photo-border",
-    "infrared",
-    "night-vision",
-    "warhol",
-    "selective-color",
-    "mirror-h",
-    "mirror-v"
+    'none',
+    'pencil',
+    'watercolor',
+    'emboss',
+    'colored-pencil',
+    'chalkboard',
+    'colored-chalkboard',
+    'airbrush',
+    'hallucination',
+    'flannel',
+    'low-ink-v',
+    'low-ink-h',
+    'collage',
+    'mosaic',
+    'photo-border',
+    'infrared',
+    'night-vision',
+    'warhol',
+    'selective-color',
+    'mirror-h',
+    'mirror-v'
   ]),
   width: PropTypes.string,
   height: PropTypes.string
-};
+}
 
 ImageEffect.defaultProps = {
-  effect: "none",
-  width: "600px",
-  height: "300px"
-};
+  effect: 'none',
+  width: '600px',
+  height: '300px'
+}
 
-export default ImageEffect;
+export default ImageEffect
