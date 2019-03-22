87d8323db0915f081adf15fd588f8402337df126
cdrani <cdrani@users.noreply.github.com>
2019-03-22 13:06:46 -0600

    add colored-pencil effect doc

---
+++ src/effects/colored-pencil/ColoredPencil.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { createImageComponent } from '../../utils'
+
+/**
+ * @visibleName colored-pencil
+ */
+
+const ImageEffect = ({ url, effect, width, height }) => {
+  const Image = createImageComponent(width, height, url, effect)
+  return <Image />
+}
+
+ImageEffect.propTypes = {
+  /** local or remote path to image */
+  url: PropTypes.string.isRequired,
+  /** image effects */
+  effect: PropTypes.oneOf([
+    'airbrush',
+    'chalkboard',
+    'collage',
+    'colored-chalkboard',
+    'colored-pencil',
+    'emboss',
+    'flannel',
+    'hallucination',
+    'infrared',
+    'low-ink-h',
+    'low-ink-v',
+    'mirror-h',
+    'mirror-v',
+    'mosaic',
+    'night-vision',
+    'none',
+    'pencil',
+    'photo-border',
+    'selective-color',
+    'warhol',
+    'watercolor'
+  ]),
+  /** width of image/component */
+  width: PropTypes.string,
+  /** width of image/component */
+  height: PropTypes.string
+}
+
+ImageEffect.defaultProps = {
+  effect: 'none',
+  width: '600px',
+  height: '300px'
+}
+
+export default ImageEffect
---
+++ src/effects/colored-pencil/ColoredPencil.md
@@ -0,0 +1,10 @@
+ColoredPencil Effect
+
+```jsx
+<ImageEffect
+  url="https://picsum.photos/900/300"
+  effect="colored-pencil"
+  width="900px"
+  height="300px"
+/>
+```
