2b8cd060e0c11074a45dff0d1bba6418a790fe12
cdrani <cdrani@users.noreply.github.com>
2019-03-23 16:04:18 -0600

    refactor stories

--- stories/index.js
+++ stories/index.js
@@ -1,174 +1,62 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
 import ImageEffect from '../src/'
 
-storiesOf('ImageEffect', module)
-  .addDecorator(storyFn => <div style={{ margin: '0 auto' }}>{storyFn()}</div>)
-  .add('airbrush', () => (
+const stories = storiesOf('ImageEffect', module)
+stories.addDecorator(
+  withInfo({
+    propTablesExclude: [ImageEffect],
+    inline: true,
+    maxPropObjectKeys: 4,
+    maxPropsIntoLine: 4
+  })
+)
+
+const effects = [
+  'airbrush',
+  'chalkboard',
+  'collage',
+  'colored-chalkboard',
+  'colored-pencil',
+  'emboss',
+  'flannel',
+  'hallucination',
+  'infrared',
+  'low-ink-h',
+  'low-ink-v',
+  'mirror-h',
+  'mirror-v',
+  'mosaic',
+  'night-vision',
+  'none',
+  'pencil',
+  'photo-border',
+  'selective-color',
+  'warhol',
+  'watercolor'
+]
+
+stories.addDecorator(storyFn => (
+  <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignContent: 'center',
+      margin: '0 auto'
+    }}
+  >
+    {storyFn()}
+  </div>
+))
+
+effects.forEach(effect => {
+  stories.add(effect, () => (
     <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='airbrush'
-      width='900px'
-      height='300px'
+      url='https://picsum.photos/900/500'
+      effect={effect}
+      width='100%'
+      height='350px'
     />
   ))
-  .add('chalkboard', () => (
-    <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='chalkboard'
-      width='900px'
-      height='300px'
-    />
-  ))
-  .add('collage', () => (
-    <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='collage'
-      width='900px'
-      height='300px'
-    />
-  ))
-  .add('colored-chalkboard', () => (
-    <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='colored-chalkboard'
-      width='900px'
-      height='300px'
-    />
-  ))
-  .add('colored-pencil', () => (
-    <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='colored-pencil'
-      width='900px'
-      height='300px'
-    />
-  ))
-  .add('emboss', () => (
-    <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='emboss'
-      width='900px'
-      height='300px'
-    />
-  ))
-  .add('flannel', () => (
-    <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='flannel'
-      width='900px'
-      height='300px'
-    />
-  ))
-  .add('hallucination', () => (
-    <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='hallucination'
-      width='900px'
-      height='300px'
-    />
-  ))
-  .add('infrared', () => (
-    <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='infrared'
-      width='900px'
-      height='300px'
-    />
-  ))
-  .add('low-ink-h', () => (
-    <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='low-ink-h'
-      width='900px'
-      height='300px'
-    />
-  ))
-  .add('low-ink-v', () => (
-    <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='low-ink-v'
-      width='900px'
-      height='300px'
-    />
-  ))
-  .add('mirror-h', () => (
-    <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='mirror-h'
-      width='900px'
-      height='300px'
-    />
-  ))
-  .add('mirror-v', () => (
-    <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='mirror-v'
-      width='900px'
-      height='300px'
-    />
-  ))
-  .add('mosaic', () => (
-    <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='mosaic'
-      width='900px'
-      height='300px'
-    />
-  ))
-  .add('night-vision', () => (
-    <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='night-vision'
-      width='900px'
-      height='300px'
-    />
-  ))
-  .add('none', () => (
-    <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='none'
-      width='900px'
-      height='300px'
-    />
-  ))
-  .add('pencil', () => (
-    <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='pencil'
-      width='900px'
-      height='300px'
-    />
-  ))
-  .add('photo-border', () => (
-    <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='photo-border'
-      width='900px'
-      height='300px'
-    />
-  ))
-  .add('selective-color', () => (
-    <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='selective-color'
-      width='900px'
-      height='300px'
-    />
-  ))
-  .add('warhol', () => (
-    <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='warhol'
-      width='900px'
-      height='300px'
-    />
-  ))
-  .add('watercolor', () => (
-    <ImageEffect
-      url='https://picsum.photos/900/300'
-      effect='watercolor'
-      width='900px'
-      height='300px'
-    />
-  ))
+})
